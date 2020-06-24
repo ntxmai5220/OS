bbd68f88c893c0d3a98c00007241b08277aa9ec2
ntxmai5220 <62063015+ntxmai5220@users.noreply.github.com>
2020-06-23 23:48:24 -0400

    Update queue.c

--- src/queue.c
+++ src/queue.c
@@ -10,8 +10,7 @@ int empty(struct queue_t *q)
 void enqueue(struct queue_t *q, struct pcb_t *proc)
 {
 	/* TODO: put a new process to queue [q] */
-	if (q->size == MAX_QUEUE_SIZE)
-		return;
+	if (q->size == MAX_QUEUE_SIZE) return;
 	q->proc[q->size] = proc;
 	q->size++;
 }
@@ -21,26 +20,24 @@ struct pcb_t *dequeue(struct queue_t *q)
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	if (q->size == 0)
-		return NULL;
+	if (q->size == 0) return NULL;
 	int sizeOfQueue = q->size;
 	struct pcb_t *proc_priority = q->proc[0];
-	int counter = 1;
 	int ind_priority = 0;
-	while (counter < sizeOfQueue)
+	for (int counter = 1; counter < sizeOfQueue; counter ++)
 	{
 		if (q->proc[counter]->priority > proc_priority->priority)
 		{
 			proc_priority = q->proc[counter];
 			ind_priority = counter;
 		}
-		counter++;
 	}
-	while (ind_priority < sizeOfQueue - 1)
+	for (; ind_priority < sizeOfQueue - 1; ind_priority++)
 	{
 		q->proc[ind_priority] = q->proc[ind_priority + 1];
-		ind_priority++;
+		
 	}
-	delete (q->proc[ind_priority]);
+	q->proc[ind_priority] = NULL;
+	q->size--;
 	return proc_priority;
 }
